21f8a2aac517e9b364d973cdcfdca574ff09b82f
Zaenal Muttakin <zmuttakin000@gmail.com>
2026-06-07 20:51:00 +0700

    Update: Add dynamic browser selection support (Chrome, Firefox, Edge) in Hooks.java

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -9,10 +9,14 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 
 /**
  * Cucumber Hooks for WebDriver lifecycle management.
- * Before: Initializes ChromeDriver with WebDriverManager.
+ * Before: Initializes WebDriver (Chrome, Firefox, or Edge) with WebDriverManager.
  * After: Takes screenshot on failure and quits the driver.
  */
 public class Hooks {
@@ -21,16 +25,38 @@ public class Hooks {
 
     @Before
     public void setUp() {
-        // Auto-download and setup ChromeDriver
-        WebDriverManager.chromedriver().setup();
+        String browser = System.getProperty("browser", "chrome").toLowerCase();
 
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new");
-        options.addArguments("--window-size=1920,1080");
-        options.addArguments("--disable-notifications");
-        options.addArguments("--remote-allow-origins=*");
+        switch (browser) {
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.addArguments("-headless");
+                firefoxOptions.addArguments("--width=1920");
+                firefoxOptions.addArguments("--height=1080");
+                driver = new FirefoxDriver(firefoxOptions);
+                break;
 
-        driver = new ChromeDriver(options);
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                EdgeOptions edgeOptions = new EdgeOptions();
+                edgeOptions.addArguments("--headless=new");
+                edgeOptions.addArguments("--window-size=1920,1080");
+                edgeOptions.addArguments("--disable-notifications");
+                driver = new EdgeDriver(edgeOptions);
+                break;
+
+            case "chrome":
+            default:
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--headless=new");
+                chromeOptions.addArguments("--window-size=1920,1080");
+                chromeOptions.addArguments("--disable-notifications");
+                chromeOptions.addArguments("--remote-allow-origins=*");
+                driver = new ChromeDriver(chromeOptions);
+                break;
+        }
     }
 
     @After
